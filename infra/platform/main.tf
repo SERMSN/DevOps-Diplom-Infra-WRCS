e17a36c0960deaa90897bd950b02578a215b05b2
WRCS <sermsn@gmail.com>
2026-04-05 23:41:02 +0700

    Apply terraform fmt for infra platform

--- infra/platform/main.tf
+++ infra/platform/main.tf
@@ -43,38 +43,38 @@ module "registry" {
 }
 
 module "kubernetes" {
-  source                   = "./modules/kubernetes"
-  folder_id                = var.yc_folder_id
-  cluster_name             = var.cluster_name
-  cluster_description      = var.cluster_description
-  cluster_version          = var.cluster_version
-  release_channel          = var.release_channel
-  network_id               = module.network.network_id
-  master_locations         = module.network.subnet_locations
+  source              = "./modules/kubernetes"
+  folder_id           = var.yc_folder_id
+  cluster_name        = var.cluster_name
+  cluster_description = var.cluster_description
+  cluster_version     = var.cluster_version
+  release_channel     = var.release_channel
+  network_id          = module.network.network_id
+  master_locations    = module.network.subnet_locations
   master_security_group_ids = [
     module.security_groups.cluster_nodegroup_traffic_security_group_id,
     module.security_groups.kubernetes_api_security_group_id
   ]
-  service_account_id       = module.service_accounts.service_account_id
-  node_service_account_id  = module.service_accounts.service_account_id
-  kms_key_id               = module.kms.key_id
-  cluster_ipv4_range       = var.cluster_ipv4_range
-  service_ipv4_range       = var.service_ipv4_range
-  master_public_ip         = var.master_public_ip
-  create_node_group        = var.create_node_group
-  node_group_name          = var.node_group_name
-  node_group_description   = var.node_group_description
-  node_count               = var.node_count
-  node_platform_id         = var.node_platform_id
-  node_cores               = var.node_cores
-  node_memory              = var.node_memory
-  node_core_fraction       = var.node_core_fraction
-  node_disk_type           = var.node_disk_type
-  node_disk_size           = var.node_disk_size
-  node_preemptible         = var.node_preemptible
-  node_subnet_ids          = module.network.subnet_ids
-  node_locations           = module.network.subnet_locations
-  node_security_group_ids  = [
+  service_account_id      = module.service_accounts.service_account_id
+  node_service_account_id = module.service_accounts.service_account_id
+  kms_key_id              = module.kms.key_id
+  cluster_ipv4_range      = var.cluster_ipv4_range
+  service_ipv4_range      = var.service_ipv4_range
+  master_public_ip        = var.master_public_ip
+  create_node_group       = var.create_node_group
+  node_group_name         = var.node_group_name
+  node_group_description  = var.node_group_description
+  node_count              = var.node_count
+  node_platform_id        = var.node_platform_id
+  node_cores              = var.node_cores
+  node_memory             = var.node_memory
+  node_core_fraction      = var.node_core_fraction
+  node_disk_type          = var.node_disk_type
+  node_disk_size          = var.node_disk_size
+  node_preemptible        = var.node_preemptible
+  node_subnet_ids         = module.network.subnet_ids
+  node_locations          = module.network.subnet_locations
+  node_security_group_ids = [
     module.security_groups.cluster_nodegroup_traffic_security_group_id,
     module.security_groups.public_services_security_group_id,
     module.security_groups.ssh_security_group_id

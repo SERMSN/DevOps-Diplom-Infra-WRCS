8ff9a22d4ccaf31e162065f8c9800157b5d1075c
WRCS <sermsn@gmail.com>
2026-04-09 21:31:50 +0700

    Edit cluster_description

--- infra/platform/variables.tf
+++ infra/platform/variables.tf
@@ -69,7 +69,7 @@ variable "cluster_name" {
 variable "cluster_description" {
   description = "Managed Kubernetes cluster description"
   type        = string
-  default     = "Managed Kubernetes cluster for Netology diploma WRCS-TEST"
+  default     = "Managed Kubernetes cluster for Netology diploma"
 }
 
 variable "cluster_version" {
